06c26b969d54fdbce295a3f8129bb2521dc9cea2
JohnyYev <zikunove@gmail.com>
2025-03-26 11:38:29 +0200

    Update hw03 Evelope

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,15 +1,15 @@
 fn main() {
-    const W: u32 = 25;
-    const H: u32 = 10;
+    const W: u32 = 40;
+    const H: u32 = 20;
 
     for y in 0..H {
         for x in 0..W {
             let is_hor: bool = y == 0 || y == H - 1;
             let is_ver: bool = x == 0 || x == W - 1;
-            let is_diag: bool = x == y;
-            let is_diag2: bool = y == W - 1 - x;
-            let to_show: bool = is_hor || is_ver || is_diag || is_diag2;
+            let is_diag1: bool = x == (y * (W - 1)) / (H - 1);
+            let is_diag2: bool = x == (W - 1) - (y * (W - 1)) / (H - 1);
 
+            let to_show: bool = is_hor || is_ver || is_diag1 || is_diag2;
             let sym: char = if to_show { '*' } else { ' ' };
             print!("{}", sym);
         }
